ce8fee4a10bcc28183a3f8c1d809429491e595cd
Julien Dumazert <julien.dumazert@alice-bob.com>
2023-09-10 12:17:19 +0200

    Add processor utils

---
+++ qiskit_alice_bob_provider/processor/utils.py
@@ -0,0 +1,151 @@
+##############################################################################
+# Copyright 2023 Alice & Bob
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+##############################################################################
+
+from typing import Union
+
+import numpy as np
+
+
+def pauli_errors_to_chi(pauli_errors: dict[str, float]) -> np.ndarray:
+    """Convert a collection of Pauli errors into a quantum process tomography
+    Chi matrix.
+
+    https://qiskit.org/documentation/stubs/qiskit.quantum_info.Chi.html
+
+    Args:
+        pauli_errors (dict[str, float]): a dict of Pauli errors where the key
+        is the type of Pauli error as a string (e.g., 'I', 'X', 'XI', 'XZX')
+        and the value is the probability of this Pauli error.
+        The "no error" case (e.g., 'I', 'II', 'III', etc) should not be
+        provided as it will be computed automatically by difference.
+
+
+    Returns:
+        np.ndarray: a quantum process tomography Chi matrix. The Qiskit link
+        above is a good description of a Chi matrix.
+    """
+    some_pauli_label = next(iter(pauli_errors))
+    n_qubits = len(some_pauli_label)
+    diag = np.zeros(shape=(4**n_qubits,), dtype=float)
+    for pauli_label, prob in pauli_errors.items():
+        diag[pauli_label_to_index(pauli_label)] = prob
+
+    # Computation of the probability of the "no error" case by difference
+    diag[0] = 1.0 - np.sum(diag[1:])
+
+    # Sanity check that all probabilities are between 0 and 1
+    try:
+        assert np.all(diag >= 0)
+    except AssertionError as e:
+        raise ValueError(
+            'Pauli error probabilities are not in [0, 1] or sum up to more'
+            f' than 1. Probabilities: {pauli_errors}'
+        ) from e
+
+    return np.diag(diag)
+
+
+def is_diagonal(m: np.ndarray) -> bool:
+    """Test if a matrix is diagonal"""
+    return np.count_nonzero(m - np.diag(np.diag(m))) == 0
+
+
+def chi_to_pauli_errors(chi: np.ndarray) -> dict[str, float]:
+    """Convert a quantum process tomography Chi matrix into a collection of
+    Pauli errors.
+
+    https://qiskit.org/documentation/stubs/qiskit.quantum_info.Chi.html
+
+    Args:
+        chi (np.ndarray): a quantum process tomography Chi matrix. The Qiskit
+        link above is a good description of a Chi matrix.
+
+    Returns:
+        np.ndarray: a quantum process tomography Chi matrix. The Qiskit link
+        above is a good description of a Chi matrix.
+        dict[str, float]: a dict of Pauli errors where the key
+        is the type of Pauli error as a string (e.g., 'I', 'X', 'XI', 'XZX')
+        and the value is the probability of this Pauli error.
+    """
+    n_qubits = int(np.round(np.log(chi.shape[0]) / np.log(4)))
+    out = {}
+    chi_diag = np.diag(chi)
+    indices = np.nonzero(chi_diag)[0]
+    for idx in indices:
+        out[index_to_pauli_label(n_qubits=n_qubits, index=idx)] = chi_diag[idx]
+    return out
+
+
+def pauli_label_to_index(pauli_str: str) -> int:
+    label_to_int = dict(zip('IXYZ', range(4)))
+    sum = 0
+    for i, c in enumerate(pauli_str[::-1]):
+        try:
+            sum += label_to_int[c] * (4**i)
+        except KeyError as e:
+            raise ValueError(
+                f'Unrecognized Pauli error label "{pauli_str}"'
+            ) from e
+    return sum
+
+
+def index_to_pauli_label(n_qubits: int, index: int) -> str:
+    paulis = 'IXYZ'
+    label = ''
+    for i in reversed(range(n_qubits)):
+        factor = 4**i
+        term = index // factor
+        label += paulis[term]
+        index -= term * factor
+    return label
+
+
+def full_flip_error(
+    linearized_probs: Union[np.ndarray, list[float], list[list[float]]]
+) -> np.ndarray:
+    """The general formula for the probabilities of Pauli errors from their
+    linearized versions in the region close to 0.
+
+    Assume pX ~ rX * t, pY ~ rY * t, and pZ ~ rZ * t when t close to 0.
+
+    Since Pauli errors combine in the following ways: X then Y is equivalent to
+    Z, X then Z to Y, etc,
+    it follows that we can build a system of first-order linear differential
+    equations relating pX(t), pX'(t), pY(t), pY'(t), pZ(t), pZ'(t).
+
+    Solving it, we get the following formula for pX:
+    pX(t) = 0.25 * (1 + exp(- 2 * (rY + rZ) * t)
+                      - exp(- 2 * (rX + rZ) * t)
+                      - exp(- 2 * (rX + rY) * t))
+    and symmetric formulas for pY and pZ.
+
+    This function implements these formulas."""
+    linearized_probs = np.atleast_2d(linearized_probs)
+    exps = np.exp(
+        -2
+        * (
+            np.roll(linearized_probs, 1, axis=1)
+            + np.roll(linearized_probs, 2, axis=1)
+        )
+    )
+    return np.clip(
+        0.25
+        * np.squeeze(
+            1 + exps - np.roll(exps, 1, axis=1) - np.roll(exps, 2, axis=1)
+        ),
+        0,
+        1,
+    )
---
+++ tests/processor/test_utils.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pytest
+
+from qiskit_alice_bob_provider.processor.utils import (
+    full_flip_error,
+    index_to_pauli_label,
+    pauli_errors_to_chi,
+    pauli_label_to_index,
+)
+
+
+def test_1_qubit_gate() -> None:
+    d = {'X': 0.3, 'Y': 0.2, 'Z': 0.4}
+    chi = pauli_errors_to_chi(d)
+    diag = np.diag(chi)
+
+    # Check Pauli errors
+    assert diag[0] == pytest.approx(0.1)
+    assert diag[1] == pytest.approx(0.3)
+    assert diag[2] == pytest.approx(0.2)
+    assert diag[3] == pytest.approx(0.4)
+
+    # Check off-diagonals are all zeros
+    for i in range(chi.shape[0]):
+        chi[i, i] = 0
+    assert np.all(chi == 0)
+
+
+def test_2_qubit_gate() -> None:
+    d = {
+        'XI': 0.3,
+        'IY': 0.2,
+        'ZY': 0.4,
+        'II': 0.2  # II is wrong but this should not matter because it is
+        # recomputed anyway by pauli_errors_to_chi
+    }
+    chi = pauli_errors_to_chi(d)
+    diag = np.diag(chi)
+
+    # Check Pauli errors
+    assert diag[0] == pytest.approx(0.1)
+    assert diag[4] == pytest.approx(0.3)
+    assert diag[2] == pytest.approx(0.2)
+    assert diag[14] == pytest.approx(0.4)
+
+    # Check off-diagonals are all zeros
+    for i in range(chi.shape[0]):
+        chi[i, i] = 0
+    assert np.all(chi == 0)
+
+
+def test_bad_probabilities() -> None:
+    with pytest.raises(ValueError):
+        pauli_errors_to_chi({'XI': 0.5, 'ZY': 0.6})
+    with pytest.raises(ValueError):
+        pauli_errors_to_chi({'XI': 0.5, 'ZY': -0.1})
+
+
+def test_bad_label() -> None:
+    with pytest.raises(ValueError):
+        pauli_errors_to_chi({'A': 0.2})
+
+
+@pytest.mark.parametrize('label', ['ZI', 'YYZ', 'I', 'XZX'])
+def test_label_conversion(label: str) -> None:
+    n_qubits = len(label)
+    assert label == index_to_pauli_label(n_qubits, pauli_label_to_index(label))
+
+
+def test_flip_error() -> None:
+    # extreme case: one of rx * t, ry * t, rz * t is large,
+    # the two others are zero
+    for idx in range(3):
+        input, output = [0.0] * 3, [0.0] * 3
+        input[idx] = 1e10
+        output[idx] = 0.5
+        assert np.array_equiv(full_flip_error(input), output)
+
+    # extreme case: t is 0
+    assert np.array_equiv(full_flip_error([0.0, 0.0, 0.0]), [0, 0, 0])
+
+    # extreme case: all are large
+    assert np.array_equiv(
+        full_flip_error([1e10, 1e10, 1e10]), [0.25, 0.25, 0.25]
+    )
+
+    input_2d = [
+        [1e10, 0, 0],
+        [0, 1e10, 0],
+    ]
+    output_2d = [
+        [0.5, 0, 0],
+        [0, 0.5, 0],
+    ]
+    assert np.array_equiv(full_flip_error(input_2d), output_2d)
+
+    # extreme case: all are very small, they don't influence each other
+    input = [1e-12, 1e-13, 1e-14]
+    expected = [1e-12, 1e-13, 1e-14]
+    out = full_flip_error(input)
+    for i in range(3):
+        assert out[i] == pytest.approx(expected[i])
