183d640e1b8d52efc5451e7f041e20283d0f8c79
Julien Dumazert <julien.dumazert@alice-bob.com>
2023-09-10 12:24:48 +0200

    Add ProcessorInstructionDurations

---
+++ qiskit_alice_bob_provider/local/instruction_durations.py
@@ -0,0 +1,205 @@
+##############################################################################
+# Copyright 2023 Alice & Bob
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+##############################################################################
+
+import warnings
+from typing import Any, Iterable, List, Optional, Tuple, Union
+
+from qiskit.circuit import Barrier, Delay, Instruction, Parameter, Qubit
+from qiskit.transpiler.instruction_durations import (
+    InstructionDurations,
+    InstructionDurationsType,
+)
+
+from ..processor.description import ProcessorDescription
+from .proc_to_qiskit import processor_to_qiskit_instruction
+
+
+class ProcessorInstructionDurations(InstructionDurations):
+    """A more flexible version of InstructionDurations.
+
+    InstructionDurations stores a duration for every supported combination of
+    instruction (a name), qubits, and params. In this sense, it is some kind of
+    fancy dict.
+
+    This makes it impossible to represent a continuous relation between
+    parameters and duration. For instance, InstructionDurations cannot
+    represent a rotation around the Z-axis rz(theta) whose duration
+    continuously depends on the angle theta.
+
+    ProcessorInstructionDurations on the other hand accepts any parameter in
+    :method:`ProcessorInstructionDurations.get` and forwards the requested
+    parameters to the underlying ProcessorDuration.
+    """
+
+    def __init__(self, processor: ProcessorDescription):
+        super().__init__(instruction_durations=None, dt=processor.clock_cycle)
+        self._proc = processor
+        self._qiskit_to_proc_mapping = _from_qiskit_to_proc_instructions(
+            processor
+        )
+
+    def get(
+        self,
+        inst: Union[str, Instruction],
+        qubits: Union[int, List[int], Qubit, List[Qubit]],
+        unit: str = 'dt',
+        parameters: Optional[List[float]] = None,
+    ) -> float:
+        """Get the duration of the instruction with the name, qubits, and
+        parameters.
+
+        Instructions may have a parameter-dependent durations
+
+        Args:
+            inst: An instruction or its name to be queried.
+            qubits: Qubits or its indices that the instruction acts on.
+            unit: The unit of duration to be returned. It must be 's' or 'dt'.
+            parameters: The value of the parameters of the desired instruction.
+
+        Returns:
+            float: The duration of the instruction on the qubits.
+
+        Raises:
+            TranspilerError: No duration is defined for the instruction.
+        """
+        if isinstance(inst, Barrier):
+            return 0
+        elif isinstance(inst, Delay):
+            with warnings.catch_warnings():
+                warnings.simplefilter('ignore')
+                converted = self._convert_unit(inst.duration, inst.unit, unit)
+            if converted == 0:
+                warnings.warn(
+                    (
+                        f'Duration is rounded to {converted} [dt] from'
+                        f' {inst.duration} [{inst.unit}]'
+                    ),
+                    UserWarning,
+                )
+            return converted
+
+        if isinstance(inst, Instruction):
+            inst_name = inst.name
+        else:
+            inst_name = inst
+
+        if isinstance(qubits, (int, Qubit)):
+            qubits = [qubits]
+
+        if isinstance(qubits[0], Qubit):
+            # This explicit for loop pleases mypy (as opposed to a
+            # for-comprehension).
+            new_qubits = []
+            for q in qubits:
+                assert isinstance(q, Qubit)
+                new_qubits.append(q.index)
+            qubits = new_qubits
+
+        if parameters is None and isinstance(inst, Instruction):
+            parameters = inst.params
+
+        with warnings.catch_warnings():
+            # We expect some rounding when going from s to dt, so we simply
+            # ignore those.
+            warnings.simplefilter('ignore')
+            return self._get(inst_name, qubits, unit, parameters)
+
+    def _get(
+        self,
+        name: str,
+        qubits: list[int],
+        to_unit: str,
+        parameters: Optional[Iterable[float]] = None,
+    ) -> float:
+        if name == 'barrier':
+            return 0
+
+        parameters = list(parameters) if parameters else []
+
+        try:
+            candidate_instructions = self._qiskit_to_proc_mapping[
+                (name, tuple(qubits))
+            ]
+        except KeyError as e:
+            raise ValueError(
+                _instruction_not_found(name, qubits, parameters)
+            ) from e
+
+        for instr, proc_instr_name in candidate_instructions:
+            if _have_same_params(
+                requested=parameters, definition=instr.params
+            ):
+                applied_instr = self._proc.apply_instruction(
+                    name=proc_instr_name,
+                    qubits=tuple(qubits),
+                    params=parameters,
+                )
+                return self._convert_unit(applied_instr.duration, 's', to_unit)
+
+        raise ValueError(_instruction_not_found(name, qubits, parameters))
+
+    def update(
+        self,
+        inst_durations: Optional[InstructionDurationsType],
+        dt: float = None,  # type: ignore
+    ):
+        """ProcessorInstructionDurations does not support updating its content
+        like InstructionDurations. It is not a dict."""
+        return NotImplementedError()
+
+
+def _instruction_not_found(
+    name: str, qubits: list[int], parameters: list[float]
+) -> str:
+    return (
+        f'The processor does not support instruction "{name}" on qubits'
+        f' {qubits} with params "{parameters}"'
+    )
+
+
+_InstructionDict = dict[
+    Tuple[str, Tuple[int, ...]], list[Tuple[Instruction, str]]
+]
+
+
+def _from_qiskit_to_proc_instructions(
+    processor: ProcessorDescription,
+) -> _InstructionDict:
+    """Create a dict mapping (Qiskit instruction name, qubits) to
+    (Qiskit instruction, proc instruction name).
+    """
+    out: _InstructionDict = {}
+    for instr in processor.all_instructions():
+        qiskit_instr = processor_to_qiskit_instruction(instr)
+        key = (str(qiskit_instr.name), instr.qubits)
+        if key not in out:
+            out[key] = []
+        out[key].append((qiskit_instr, instr.name))
+    return out
+
+
+def _have_same_params(requested: list[Any], definition: list[Any]) -> bool:
+    """Compare the parameters provided by the Qiskit instruction in the circuit
+    with the parameters of a reference Qiskit instruction."""
+    if len(requested) != len(definition):
+        return False
+    for req, def_ in zip(requested, definition):
+        if isinstance(def_, Parameter):
+            continue
+        if req == def_:
+            continue
+        return False
+    return True
---
+++ tests/local/test_instruction_durations.py
@@ -0,0 +1,114 @@
+# pylint: disable=redefined-outer-name
+
+from typing import Iterator, Tuple
+
+import pytest
+from qiskit.circuit import Barrier, Delay
+from qiskit.circuit.library import CCXGate, RXGate, XGate
+
+from qiskit_alice_bob_provider.local.instruction_durations import (
+    ProcessorInstructionDurations,
+)
+from qiskit_alice_bob_provider.processor.description import (
+    AppliedInstruction,
+    InstructionProperties,
+    ProcessorDescription,
+)
+
+
+class _TestProcessor(ProcessorDescription):
+    def __init__(self, clock_cycle: float):
+        self.clock_cycle = clock_cycle
+        self._delay_qubits: list[Tuple[int, ...]] = [(0,), (1,)]
+        self._ccx_qubits: list[Tuple[int, ...]] = [(0, 1, 2), (0, 2, 1)]
+        self._rx_qubits: list[Tuple[int, ...]] = [(2,)]
+
+    def all_instructions(self) -> Iterator[InstructionProperties]:
+        for tup in self._delay_qubits:
+            yield InstructionProperties(
+                name='delay', params=['duration'], qubits=tup
+            )
+        for tup in self._ccx_qubits:
+            yield InstructionProperties(name='ccx', params=[], qubits=tup)
+        for tup in self._rx_qubits:
+            yield InstructionProperties(
+                name='rx', params=['angle'], qubits=tup
+            )
+
+    def apply_instruction(
+        self, name: str, qubits: Tuple[int, ...], params: list[float]
+    ) -> AppliedInstruction:
+        if name == 'delay':
+            return AppliedInstruction(
+                duration=params[0], quantum_errors=None, readout_errors=None
+            )
+        elif name == 'ccx':
+            if qubits == self._ccx_qubits[0]:
+                return AppliedInstruction(
+                    duration=0.1, quantum_errors=None, readout_errors=None
+                )
+            else:
+                return AppliedInstruction(
+                    duration=0.2, quantum_errors=None, readout_errors=None
+                )
+        elif name == 'rx':
+            if qubits == self._rx_qubits[0]:
+                return AppliedInstruction(
+                    duration=params[0] * 2,
+                    quantum_errors=None,
+                    readout_errors=None,
+                )
+        raise NotImplementedError()
+
+
+@pytest.fixture
+def instr_dur() -> ProcessorInstructionDurations:
+    return ProcessorInstructionDurations(_TestProcessor(1e-3))
+
+
+def test_bad_input(instr_dur: ProcessorInstructionDurations) -> None:
+    with pytest.raises(ValueError):  # bad instruction
+        instr_dur.get(inst=XGate(), qubits=0)
+    with pytest.raises(ValueError):  # bad qubits
+        instr_dur.get(inst=CCXGate(), qubits=(0, 1, 3))
+
+
+def test_instruction_as_string(
+    instr_dur: ProcessorInstructionDurations,
+) -> None:
+    assert instr_dur.get(inst='ccx', qubits=(0, 1, 2), unit='s') == 0.1
+
+
+def test_barrier(
+    instr_dur: ProcessorInstructionDurations,
+) -> None:
+    assert instr_dur.get(inst='barrier', qubits=(0, 1, 2), unit='s') == 0.0
+    assert instr_dur.get(inst=Barrier(3), qubits=(0, 1, 2), unit='s') == 0.0
+
+
+def test_qubits_forwarded_to_processor(
+    instr_dur: ProcessorInstructionDurations,
+) -> None:
+    assert instr_dur.get(inst=CCXGate(), qubits=(0, 1, 2), unit='s') == 0.1
+    assert instr_dur.get(inst=CCXGate(), qubits=(0, 2, 1), unit='s') == 0.2
+
+
+def test_time_unit_conversion(
+    instr_dur: ProcessorInstructionDurations,
+) -> None:
+    assert instr_dur.get(inst=Delay(2, 's'), qubits=(0,), unit='s') == 2
+    assert instr_dur.get(inst=Delay(2, 'dt'), qubits=(0,), unit='s') == 2e-3
+    assert instr_dur.get(inst=Delay(2, 'dt'), qubits=(0,), unit='dt') == 2
+    with pytest.warns(UserWarning):
+        assert instr_dur.get(inst=Delay(2, 'us'), qubits=(0,), unit='dt') == 0
+    assert instr_dur.get(inst=Delay(2000, 'us'), qubits=(0,), unit='dt') == 2
+
+
+def test_instruction_with_params_not_delay(
+    instr_dur: ProcessorInstructionDurations,
+) -> None:
+    assert (
+        instr_dur.get(inst=RXGate(0.2), qubits=2, parameters=[0.3], unit='s')
+        == 0.6
+    )
+    assert instr_dur.get(inst=RXGate(0.2), qubits=2, unit='s') == 0.4
