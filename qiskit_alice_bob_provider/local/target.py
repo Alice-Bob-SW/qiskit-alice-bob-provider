9b0d8fa203d5ee0eb8dff31adccb1d29fc97c847
Julien Dumazert <julien.dumazert@alice-bob.com>
2023-09-10 12:26:59 +0200

    Build Qiskit targets from ProcessorDescription

---
+++ qiskit_alice_bob_provider/local/target.py
@@ -0,0 +1,97 @@
+##############################################################################
+# Copyright 2023 Alice & Bob
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+##############################################################################
+
+from typing import Tuple
+
+from qiskit.circuit import Instruction
+from qiskit.transpiler import Target
+
+from ..processor.description import ProcessorDescription
+from .instruction_durations import ProcessorInstructionDurations
+from .proc_to_qiskit import processor_to_qiskit_instruction
+
+
+def processor_to_target(processor: ProcessorDescription) -> Target:
+    """Create a Qiskit Target from a ProcessorDescription
+
+    A ProcessorDescription is the description of the properties of a quantum
+    processor. In this regard, a processor has the same function as a Qiskit
+    Target.
+
+    Please note that the Qiskit target produced by this function does not fully
+    represent the array of operations available on the processor.
+
+    This is because, contrary to what is advertised in the Target documentation
+    at https://qiskit.org/documentation/stubs/qiskit.transpiler.Target.html,
+    the Qiskit transpiler expects the names of instructions in a Target to be
+    listed under their Qiskit operation names. Otherwise they won't be taken
+    into account and used by some transpilation passes who have a basis_gates
+    argument like UnrollCustomDefinitions.
+
+    This is not a problem for scheduling because durations are obtained from
+    the richer ProcessorInstructionDurations class which derives from
+    InstructionDurations.
+    This might be an issue for other types of transpilation passes.
+
+    Here's a concrete example of this loss of information:
+
+    If an Alice & Bob processor implements the following instructions:
+    * p0, which translates to Initialize(0)
+    * p+, which translates to Initialize (+)
+    * rz, which translates to RzGate(Parameter('angle'))
+    * rzpio2, which translates to RzGate(np.pi/2)
+
+    Then the corresponding Qiskit Target will contain the following
+    instructions:
+    * 'initialize' --> Initialize(0)
+    * 'rz' --> RzGate(Parameter('angle'))
+
+    This loss of information cannot be avoided because Qiskit identifies
+    instructions by their names for simplicity.
+
+    The last instruction (rzpio2) is a made-up example for the sake of clarity.
+    """
+    target = Target(dt=processor.clock_cycle)
+
+    # qiskit name -> supported qubit combinations
+    qubits: dict[str, list[Tuple[int, ...]]] = {}
+
+    # Qiskit name -> a given Qiskit implementation of this instruction
+    # As a consequence, if two proc instructions are implemented using the same
+    # Qiskit implementation, they will conflict here. This is not an issue
+    # though, because the Qiskit Target's instructions are only used for
+    # gate basis conversion.
+    example_instructions: dict[str, Instruction] = {}
+
+    for instruction in processor.all_instructions():
+        qiskit_instr = processor_to_qiskit_instruction(instruction)
+        name = qiskit_instr.name
+        example_instructions[name] = qiskit_instr
+        if name not in qubits:
+            qubits[name] = []
+        qubits[name].append(instruction.qubits)
+
+    for name, qiskit_instr in example_instructions.items():
+        properties = {qbts: None for qbts in qubits[name]}
+        target.add_instruction(
+            instruction=qiskit_instr,
+            properties=properties,
+            name=qiskit_instr.name,
+        )
+    target._instruction_durations = (  # pylint: disable=protected-access
+        ProcessorInstructionDurations(processor=processor)
+    )
+    return target
---
+++ tests/local/test_target.py
@@ -0,0 +1,44 @@
+from typing import Iterator, Tuple
+
+from qiskit_alice_bob_provider.local.instruction_durations import (
+    ProcessorInstructionDurations,
+)
+from qiskit_alice_bob_provider.local.target import processor_to_target
+from qiskit_alice_bob_provider.processor.description import (
+    AppliedInstruction,
+    InstructionProperties,
+    ProcessorDescription,
+)
+
+
+class _TestProcessor(ProcessorDescription):
+    def __init__(self, clock_cycle: float):
+        self.clock_cycle = clock_cycle
+
+    def all_instructions(self) -> Iterator[InstructionProperties]:
+        yield InstructionProperties(
+            name='delay', params=['duration'], qubits=(0,)
+        )
+        yield InstructionProperties(
+            name='delay', params=['duration'], qubits=(1,)
+        )
+        yield InstructionProperties(name='ccx', params=[], qubits=(0, 1, 2))
+        yield InstructionProperties(name='ccx', params=[], qubits=(0, 2, 1))
+
+    def apply_instruction(
+        self, name: str, qubits: Tuple[int, ...], params: list[float]
+    ) -> AppliedInstruction:
+        raise NotImplementedError()
+
+
+def test_target_from_processor() -> None:
+    clock_cycle = 3
+    target = processor_to_target(_TestProcessor(clock_cycle))
+    assert target.dt == clock_cycle
+    assert target.instruction_supported('delay', qargs=(0,))
+    assert target.instruction_supported('delay', qargs=(1,))
+    assert not target.instruction_supported('delay', qargs=(2,))
+    assert target.instruction_supported('ccx', qargs=(0, 1, 2))
+    assert target.instruction_supported('ccx', qargs=(0, 2, 1))
+    assert not target.instruction_supported('ccx', qargs=(3, 4, 5))
+    assert isinstance(target.durations(), ProcessorInstructionDurations)
