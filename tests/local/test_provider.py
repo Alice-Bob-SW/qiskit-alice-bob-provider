616361a811fecda04c0bfa12fc5374d8ba3f720a
Maxence Drutel <maxence.drutel@alice-bob.com>
2023-10-11 13:58:46 +0000

    Update local backend names to the defined convention

--- tests/local/test_provider.py
+++ tests/local/test_provider.py
@@ -4,7 +4,9 @@ from qiskit_alice_bob_provider.local.provider import AliceBobLocalProvider
 def test_get_backends() -> None:
     ab = AliceBobLocalProvider()
     ab.backends()
-    assert ab.get_backend('PHYSICAL_CATS_6').name == 'PHYSICAL_CATS_6'
-    backends = ab.backends('PHYSICAL_CATS_6')
+    assert (
+        ab.get_backend('EMU:6Q:PHYSICAL_CATS').name == 'EMU:6Q:PHYSICAL_CATS'
+    )
+    backends = ab.backends('EMU:6Q:PHYSICAL_CATS')
     assert len(backends) == 1
-    assert ab.get_backend('PHYSICAL_CATS_6').name == backends[0].name
+    assert ab.get_backend('EMU:6Q:PHYSICAL_CATS').name == backends[0].name
