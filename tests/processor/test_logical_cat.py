c187d8accfb51ec23a08d6a09a3a8ae641b1868b
Benjamin Lesieux <benjamin.lesieux@alice-bob.com>
2025-03-13 17:05:48 +0100

    test: write LogicalCatProcessor tests for noiseless and noisy parameters

--- tests/processor/test_logical_cat.py
+++ tests/processor/test_logical_cat.py
@@ -1,27 +1,88 @@
 import numpy as np
 import pytest
 
-from qiskit_alice_bob_provider.processor.logical_cat import LogicalCatProcessor
+from qiskit_alice_bob_provider.processor.logical_cat import (
+    NOISELESS_CAT_ERROR,
+    LogicalCatProcessor,
+)
 
 
-def test_parameter_validation() -> None:
+@pytest.mark.parametrize(
+    'arguments',
+    [
+        {'average_nb_photons': -3},
+        {'kappa_1': 2},
+        {'kappa_1': 100, 'kappa_2': 100_000_000_000},
+        {'distance': 0},
+        {'distance': 6},
+    ],
+)
+def test_parameter_validation(arguments) -> None:
     with pytest.raises(ValueError):
-        LogicalCatProcessor(average_nb_photons=-3)
-    with pytest.raises(ValueError):
-        LogicalCatProcessor(kappa_1=2)
-    with pytest.raises(ValueError):
-        LogicalCatProcessor(kappa_1=100, kappa_2=100_000_000_000)
-    with pytest.raises(ValueError):
-        LogicalCatProcessor(distance=0)
-    with pytest.raises(ValueError):
-        LogicalCatProcessor(distance=6)
-    LogicalCatProcessor(kappa_1=10)
-    LogicalCatProcessor(kappa_1=10, kappa_2=10_000)
-    LogicalCatProcessor(average_nb_photons=4)
+        LogicalCatProcessor.create_noisy(**arguments)
 
 
-def test_all_instructions() -> None:
-    proc = LogicalCatProcessor()
+@pytest.mark.parametrize(
+    'arguments',
+    [
+        {'kappa_1': 10},
+        {'kappa_1': 10, 'kappa_2': 10_000},
+        {'average_nb_photons': 4},
+    ],
+)
+def test_noisy_parameter_validator_ok(arguments) -> None:
+    LogicalCatProcessor.create_noisy(**arguments)
+
+
+@pytest.mark.parametrize(
+    'arguments',
+    [
+        {'n_qubits': 10},
+        {'clock_cycle': 1e-9},
+        {},
+    ],
+)
+def test_from_noiseless_parameter_validator_ok(arguments) -> None:
+    LogicalCatProcessor.create_noiseless(**arguments)
+
+
+@pytest.mark.parametrize(
+    'arguments',
+    [
+        {'kappa_1': 10},
+        {'kappa_1': 10, 'kappa_2': 10_000},
+        {'average_nb_photons': 4},
+    ],
+)
+def test_from_noiseless_parameter_validation_raises(arguments) -> None:
+    with pytest.raises(ValueError) as e:
+        LogicalCatProcessor.create_noiseless(**arguments)
+    assert e.value.args[0] == NOISELESS_CAT_ERROR
+
+
+@pytest.mark.parametrize(
+    'arguments',
+    [
+        {'kappa_1': 10},
+        {'kappa_1': 10, 'kappa_2': 10_000},
+        {'average_nb_photons': 4},
+    ],
+)
+def test_noiseless_parameter_validation_raises(arguments) -> None:
+    with pytest.raises(ValueError) as e:
+        LogicalCatProcessor(**arguments, noiseless=True)
+    assert e.value.args[0] == NOISELESS_CAT_ERROR
+
+
+@pytest.mark.parametrize(
+    'get_cat',
+    [
+        LogicalCatProcessor.create_noisy,
+        LogicalCatProcessor.create_noiseless,
+    ],
+)
+def test_all_instructions(get_cat) -> None:
+    proc = get_cat()
     proc.apply_instruction('mx', (0,), [])
     proc.apply_instruction('mz', (0,), [])
     proc.apply_instruction('delay', (0,), [1e-4])
@@ -40,10 +101,20 @@ def test_all_instructions() -> None:
 def test_delay_instruction_short() -> None:
     """The quantum should never be empty, even when the duration is shorter
     than an error correction cycle."""
-    proc = LogicalCatProcessor()
+    proc = (
+        LogicalCatProcessor.create_noisy()
+    )  # Equivalent to LogicalCatProcessor(), better for clarity
     error = proc.apply_instruction('delay', (0,), [1e-8]).quantum_errors
     assert error is not None
     assert error.shape == (4, 4)
+
+
+def test_delay_instruction_short_noiseless() -> None:
+    """The quantum should never be empty, even when the duration is shorter
+    than an error correction cycle."""
+    proc = LogicalCatProcessor.create_noiseless()
+    error = proc.apply_instruction('delay', (0,), [1e-8]).quantum_errors
+    assert error is None
 
 
 def test_1q_instruction() -> None:
@@ -64,3 +135,22 @@ def test_1q_instruction() -> None:
     assert diag[2] == pytest.approx(px * pz)
     assert diag[3] == pytest.approx(pz)  # first order
     assert diag.sum() == pytest.approx(1.0)
+
+
+def test_1q_instruction_noiseless() -> None:
+    # pylint: disable=protected-access
+    proc = LogicalCatProcessor.create_noiseless()
+    d, _, _, k2 = (
+        proc._distance,
+        proc._average_nb_photons,
+        proc._kappa_1,
+        proc._kappa_2,
+    )
+
+    applied = proc.apply_instruction('x', (0,), [])
+    t = 5 * d / k2
+    assert applied.duration == pytest.approx(t)
+
+    # Noiseless Cat: Not quantum or readout errors
+    assert applied.quantum_errors is None
+    assert applied.readout_errors is None
